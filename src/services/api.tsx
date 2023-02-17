5bb1d3a14ea28ed3566957added6c28367563614
menelucas <115168314+menelucas@users.noreply.github.com>
2023-02-17 17:14:05 -0300

    Dotenv - front

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -5,11 +5,13 @@ export const api = {
   get: (url: string) => {
     const cookies = new Cookies()
     const token = cookies.get('token')
-    console.log(token.accessToken)
+    const api_url = import.meta.env.VITE_API_URL
+    const api_port = import.meta.env.VITE_API_PORT
+    console.log('http://' + api_url + ':' + api_port + '/api' + url + '/')
 
     const options = {
       method: 'GET',
-      url: 'http://localhost:8080/api' + url + '/',
+      url: 'http://' + api_url + ':' + api_port + '/api' + url + '/',
       headers: {
         'Content-Type': 'application/json',
         'x-access-token': token.accessToken
